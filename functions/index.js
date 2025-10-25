f7b7da1199331fd7057f6fc2cd01e8686f598e19
inorganic26 <inorganic26@gmail.com>
2025-10-26 04:30:35 +0900

    제미나이 api제거

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,8 @@ import { onCall } from "firebase-functions/v2/https";
 import * as functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import dotenv from "dotenv";
 
-dotenv.config(); // ✅ .env 파일 자동 로드
+// dotenv import 및 config() 호출 제거됨
 
 initializeApp();
 
